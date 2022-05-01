5eb254e61fea502a0e3bbc8d99af71828e69e870
riskezwn <enriquegarciagasc@gmail.com>
2022-05-01 22:23:49 +0200

    Clean project and install NextUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { NextUIProvider } from '@nextui-org/react'
+
+import '../styles/globals.css'
+
+function MyApp ({ Component, pageProps }: AppProps) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
 
 export default MyApp
